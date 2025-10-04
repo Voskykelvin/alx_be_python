62788109e80015c4b40d891e01e8df713693a352
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-10-04 06:48:37 +0300

    Initial commit

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,7 +6,7 @@ def safe_divide(numerator, denominator):
 
         # Try division
         result = num / den
-        return f"The result is: {result}"
+        return f"The result of the division is: {result}"
 
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
